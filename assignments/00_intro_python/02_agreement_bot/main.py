03f07c4fcaf8affc8dc5d5ef91b648d9ee90c166
Aqsa_Saeed <shireensaeed786@gmail.com>
2025-03-30 00:12:55 +0500

    Delete assignments directory

--- assignments/00_intro_python/02_agreement_bot/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    user_input : str = input("What's your favorite animal?")
-    output : str = f"My favorite animal is also {user_input}!"
-    print(output)
-
-if __name__ == "__main__":
-    main()
--- assignments/00_intro_python/06_square_number/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    user_input : str = input("Type a number to see its square: \n")
-    squared_num : int = int(user_input) ** 2
-    print(f"The square of {user_input} is {squared_num}!")
-    
-
-if __name__ == "__main__":
-    main()
